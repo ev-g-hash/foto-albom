283c7dc9a320b4e1312df2e94021414eb83a2e60
ev-g-hash <pihtulovevgeny@gmail.com>
2025-12-07 16:01:56 +0400

    деплой амвера +

--- main/settings.py
+++ main/settings.py
@@ -33,13 +33,11 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    # 'whitenoise.runserver_nostatic', # раскоментить перед продакшн
+    'django.contrib.staticfiles',    
     'fotos',
 ]
 
-MIDDLEWARE = [
-    # 'whitenoise.middleware.WhiteNoiseMiddleware', # раскоментить перед продакшн
+MIDDLEWARE = [    
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
