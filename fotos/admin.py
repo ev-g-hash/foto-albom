9b5cc3e539a50a47d0e49167ac3191c0b48a680e
ev-g-hash <pihtulovevgeny@gmail.com>
2025-12-12 04:07:16 +0400

    очистка

--- fotos/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Photo
-
-@admin.register(Photo)
-class PhotoAdmin(admin.ModelAdmin):
-    list_display = ('title', 'created_at')
-    list_filter = ('created_at',)
-    search_fields = ('title', 'description')
-    date_hierarchy = 'created_at'
